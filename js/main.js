d7f6d6c4f41b78c005c81a440628677a86857786
Kwak <khsin510@naver.com>
2020-08-13 23:31:17 +0900

    modal text

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-const user = document.getElementById("user");
+const openSign = document.getElementById("open");
+const modal = document.querySelector(".modal");
+const closebtn = modal.querySelector("button");
+const overlay = modal.querySelector(".modal-overlay");
 
-
-
-
-const clicked = () => {
-    document.querySelector("#modal").style.display="block";
+const openModal = () => {
+    modal.classList.remove("hidden");
 }
-
-
-user.addEventListener("click",clicked);
-
-
 const closeModal = () =>{
-    document.querySelector("#modal").style.display="none";
+    modal.classList.add("hidden");
 }
+overlay.addEventListener("click", closeModal);
+closebtn.addEventListener("click", closeModal);
+openSign.addEventListener("click", openModal);
