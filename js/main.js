e12d267f9fb81c25628644d1e55d1f5250428133
Kwak <khsin510@naver.com>
2020-08-16 01:00:49 +0900

    200816 add modal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-const openSign = document.getElementById("open");
+const openButton = document.getElementById("open");
+const closeButton = document.getElementById("close");
 const modal = document.querySelector(".modal");
-const closebtn = modal.querySelector("button");
-const overlay = modal.querySelector(".modal-overlay");
 
-const openModal = () => {
+const openModal = () =>{
+    console.log("open modal");
     modal.classList.remove("hidden");
 }
+
 const closeModal = () =>{
+    console.log("close modal");
     modal.classList.add("hidden");
 }
-overlay.addEventListener("click", closeModal);
-closebtn.addEventListener("click", closeModal);
-openSign.addEventListener("click", openModal);
+modal.addEventListener("click",closeModal);
+openButton.addEventListener("click",openModal);
+closeButton.addEventListener("click",closeModal);
